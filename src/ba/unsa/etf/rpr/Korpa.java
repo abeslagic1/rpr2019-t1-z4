5b5bb625ef7f8a4995572b5ce95105f96a22ff44
Amar Beslagic <abeslagic1@etf.unsa.ba>
2021-10-28 23:37:19 +0200

    Dodana klasa Korpa i Supermarket.

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,48 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] stvari;
+    int vel;
+
+    public Korpa(){
+        stvari = new Artikl[50];
+        vel = 0;
+    }
+
+    public Artikl[] getArtikli() {
+        return stvari;
+    }
+
+    public boolean dodajArtikl(Artikl a){
+        if(vel < 50){
+            stvari[vel] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            vel++;
+            return true;
+        }
+        return false;
+    }
+
+    public int dajUkupnuCijenuArtikala(){
+        int ukupno = 0;
+        for(int i = 0; i < vel; i++){
+            ukupno += stvari[i].getCijena();
+        }
+        return ukupno;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String bar){
+        Artikl izbaci = null;
+        for(int i = 0; i < vel; i++){
+            if(stvari[i].kod.equals(bar)){
+                izbaci = stvari[i];
+                for(int j = i + 1; j < vel; j++){
+                    stvari[j - 1] = stvari[j];
+                }
+                stvari[vel - 1] = null;
+                --vel;
+                --i;
+            }
+        }
+        return izbaci;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,41 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli;
+    int velicina;
+
+    public Supermarket(){
+        artikli = new Artikl[1000];
+        velicina = 0;
+    }
+
+    public Artikl[] getArtikli(){
+        return artikli;
+    }
+
+    public boolean dodajArtikl(Artikl a){
+        if(velicina < 1000){
+            artikli[velicina] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            velicina++;
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String bar) {
+        Artikl izbaci = null;
+        for (int i = 0; i < velicina; i++) {
+            if (artikli[i].kod.equals(bar)) {
+                izbaci = artikli[i];
+                for (int j = i + 1; j < velicina; j++) {
+                    artikli[j - 1] = artikli[j];
+                }
+
+                artikli[velicina - 1] = null;
+                --velicina;
+                --i;
+            }
+        }
+        return izbaci;
+    }
 }
